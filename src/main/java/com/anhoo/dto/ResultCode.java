369d80380efb1d5da77f7244cab5fbf0f1b0c7b0
eoooxy <eoooxy@163.com>
2017-05-12 17:07:54 +0800

    添加错误代码，统一调用。’

---
+++ src/main/java/com/anhoo/dto/ResultCode.java
@@ -0,0 +1,14 @@
+package com.anhoo.dto;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Author XueYuan
+ * Data  2017/05/12
+ * Time  10:48
+ */
+
+public class ResultCode {
+
+    public static final String SUCCESS = "100";
+    public static final String FAIL = "200";
+}
